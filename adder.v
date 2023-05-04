929511c45e8b73e8144500531524f523a94b756e
arthurnamz <arthurmwangonda@outlook.com>
2023-05-04 03:19:44 +0200

    adding files

---
+++ adder.v
@@ -0,0 +1,10 @@
+module adder#(
+    parameter DATA_WIDTH = 32,
+    parameter ADDRESS_SIZE = 8
+)
+(
+    input ACLK  // Global clock signal
+    input ARESETn //Global reset signal, of type active LOW 
+);
+
+endmodule
