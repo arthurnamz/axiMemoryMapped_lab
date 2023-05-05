ba553e1b45a86676169eb90ea0e0997a71ee1956
arthurnamz <arthurmwangonda@outlook.com>
2023-05-05 18:35:56 +0200

    updates

--- adder.v
+++ adder.v
@@ -5,62 +5,65 @@ module adder#(
 )
 (
     // Global signals
-    input s0_axi_aclk,
-    input s0_axi_aresetn,
+    input s1_axi_aclk,
+    input s1_axi_aresetn,
 
     // Write address channel
-    input [ADDR_WIDTH-1:0] s0_axi_awaddr,
-    input s0_axi_awvalid,
-    output reg s0_axi_awready,
+    input [ADDR_WIDTH-1:0] s1_axi_awaddr,
+    input s1_axi_awvalid,
+    output reg s1_axi_awready,
 
     // Write data channel
-    input [DATA_WIDTH-1:0] s0_axi_wdata,
-    input [DATA_WIDTH/8:0] s0_axi_wstrb,
-    input s0_axi_wvalid,
-    output reg s0_axi_wready,
+    input [DATA_WIDTH-1:0] s1_axi_wdata,
+    input [DATA_WIDTH/8:0] s1_axi_wstrb,
+    input s1_axi_wvalid,
+    output reg s1_axi_wready,
 
     // Write response channel
-    output reg s0_axi_bresp,
-    output reg s0_axi_bvalid,
-    input s0_axi_bready,
+    output reg s1_axi_bresp,
+    output reg s1_axi_bvalid,
+    input s1_axi_bready,
 
     // Read address channel
-    input [ADDR_WIDTH-1:0] s0_axi_araddr,
-    input s0_axi_arvalid,
-    output reg s0_axi_arready,
+    input [ADDR_WIDTH-1:0] s1_axi_araddr,
+    input s1_axi_arvalid,
+    output reg s1_axi_arready,
 
     // Read data channel
-    output reg [DATA_WIDTH-1:0] s0_axi_rdata,
-    output reg s0_axi_rresp,
-    output reg s0_axi_rvalid,
-    input s0_axi_rready
+    output reg [DATA_WIDTH-1:0] s1_axi_rdata,
+    output reg s1_axi_rresp,
+    output reg s1_axi_rvalid,
+    input s1_axi_rready
 );
 reg [DATA_WITDH-1:0] operandA, operandB;
 reg [(2*DATA_WITDH-1):0] result_tmp;
 wire [DATA_WITDH-1:0] overflow_adder;
 
 //getting data from the master
-always@(posedge clk)
+always@(posedge s1_axi_aclk)
 begin
-    if(rstn ==0)
-	   ready <= 0;
+    if(~s1_axi_aresetn)
+	   s1_axi_awready <= 0;
+       s1_axi_wready <= 0;
 	else   
-	if(awvalid == 1 && wvalid == 1)
+	if(s1_axi_awvalid == 1 && s1_axi_wvalid == 1)
 	  begin
-	     case(awaddr)
+	     case(s1_axi_awaddr)
 		   0:
 		     begin
-		      operandA <= wdata;
-			  bresp <= 1;
+		      operandA <= s1_axi_wdata;
+			  s1_axi_bresp <= 1;
+
 			 end  
 		   4: 
 		     begin
-		      operandB <= wdata;
-			  bresp <= 1;
+		      operandB <= s1_axi_wdata;
+			  s1_axi_bresp <= 1;
 			 end 
 		   default:
 		      begin
-			     ready <= 1;
+			     s1_axi_awready <= 1;
+                 s1_axi_wready <= 1;
 			  end
 
       endcase
@@ -78,24 +81,31 @@ begin
   result_tmp <= operandA + operandB;
 end
 
-assign overflow_adder = (result_tmp > (2**DATA_WITDH)-1)?1:0;   //tri-state assignment
+assign overflow_adder = (result_tmp > (2**DATA_WIDTH)-1)?1:0;   //tri-state assignment
 
 
 //returning results to the master
-always@(posedge clk)
+always@(posedge s1_axi_aclk)
 begin
-   
-   if(arvalid)
+   if(~s1_axi_aresetn)
+	   s1_axi_awready <= 0;
+       s1_axi_wready <= 0;
+
+	else if(s1_axi_arvalid)
    begin
-      case(araddress)
+      case(s1_axi_araddr)
 	     8:
-		    rdata <= result_tmp[31:0];
+		    s1_axi_rdata <= result_tmp[31:0];
 		 12:
 		    rdata <= overflow_adder;
 	     default:
                   //do something		 
       endcase
    end
+   else
+	  begin
+	     // maybe other controls need to be set
+	  end
    
 end
 
