715397e40ecf38216f21a708bf090e27c7575f0b
arthurnamz <arthurmwangonda@outlook.com>
2023-05-15 08:03:47 +0200

    updates

--- bus.sv
+++ bus.sv
@@ -3,6 +3,8 @@ module bus#(
     parameter ADDR_WIDTH = 8
 )
 (
+
+ /* slave interface */   
     // Global signals
     input s0_axi_aclk,
     input s0_axi_aresetn,
@@ -33,6 +35,38 @@ module bus#(
     output reg s0_axi_rresp,
     output reg s0_axi_rvalid,
     input s0_axi_rready
+
+/* master interface */
+   // Global signals
+    input m1_axi_aclk,
+    input m1_axi_aresetn,
+
+    // Write address channel
+    output [ADDR_WIDTH-1:0] m1_axi_awaddr,
+    output m1_axi_awvalid,
+    input  m1_axi_awready,
+
+    // Write data channel
+    output [DATA_WIDTH-1:0] m1_axi_wdata,
+    output [DATA_WIDTH/8:0] m1_axi_wstrb,
+    output m1_axi_wvalid,
+    input  m1_axi_wready,
+
+    // Write response channel
+    input reg m1_axi_bresp,
+    input reg m1_axi_bvalid,
+    output m1_axi_bready,
+
+    // Read address channel
+    output [ADDR_WIDTH-1:0] m1_axi_araddr,
+    output m1_axi_arvalid,
+    input  m1_axi_arready,
+
+    // Read data channel
+    input reg [DATA_WIDTH-1:0] m1_axi_rdata,
+    input reg m1_axi_rresp,
+    input reg m1_axi_rvalid,
+    output  m1_axi_rready
 );
 
 endmodule
