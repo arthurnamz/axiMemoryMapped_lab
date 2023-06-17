2ccfb36d403cd6725140a7e065a4c01bb1b9d4d7
arthurnamz <arthurmwangonda@outlook.com>
2023-06-17 09:07:12 +0200

    updates

--- wrapper_tb.v
+++ wrapper_tb.v
@@ -112,13 +112,13 @@ module wrapper_tb;
     s3_axi_rready = 1;
     s3_axi_araddr = 8;
     s3_axi_arvalid = 0;
-    #250;
+    #30;
     s3_axi_rready = 0;
     #270;
     s3_axi_rready = 1;
     s3_axi_araddr = 12;
     s3_axi_arvalid = 0;
-    #290;
+    #20;
     s3_axi_rready = 0;
     
     
